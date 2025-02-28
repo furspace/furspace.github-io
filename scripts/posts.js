ec6afc7fb70fc7cae4773920991074b42dc639e7
Xavier <xvr0612@gmail.com>
2025-02-28 16:58:17 -0500

    continued work on posts

---
+++ scripts/posts.js
@@ -0,0 +1,30 @@
+var posts = [
+    {
+        "date": "May 2005",
+        "author": "Wellsley",
+        "image": "./img/posts/1.jpg",
+        "comments": [
+            {
+                "author": "Xavier",
+                "body": "hi"
+            }
+        ]
+    },
+
+]
+
+
+/*
+{
+    "title": "",
+    "date": "May 2005",
+    "author": "Wellsley",
+    "image": "./img/posts/#.jpg",
+    "comments": [
+        {
+            "author": "",
+            "body": ""
+        }
+    ]
+},
+*/
